ec1eecdca2757ae8eeeb2c1b65aec6e96b447e20
Hudson Romualdo <hudson.paula@gmail.com>
2024-03-12 18:25:34 -0300

    Ajustes gerais no código dos projetos.

--- horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
+++ horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
@@ -12,16 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/agora")
 public class AgoraServlet extends HttpServlet{
 	
+	private static final long serialVersionUID = -4512355925305510242L;
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {		
-		//Processamento de backend
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {		
+		//Processamento de backend (poderia ser um cálculo elaborado, a consulta ao banco de dados, etc)
 		Date data = new Date();
 		
 		//Armazenar valores necessários para a JSP
 		req.setAttribute("dataHora", data);
 		
-		//Retornar para a página dataHoraCerta.jsp
+		//Retornar para a página dataHoraCerta.jsp que está no mesmo contexto (projeto)
 		req.getRequestDispatcher("dataHoraCerta.jsp")
-			.forward(req, resp);
+			.forward(req, resp); //O redirecionamento está incluindo os objetos Request e Response
 	}
 }
+
